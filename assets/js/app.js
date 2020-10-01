168f3bb5d77fc819be0c93d2fcc0b876e1595628
natachasha <kafffkins@yahoo.co.uk>
2020-10-01 19:43:29 +0300

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,12 @@
 const body = document.querySelector("body"),
+	wrapper = document.querySelector(".wrapper"),
 	menu = document.querySelector(".nav"),
 	btnNav = document.querySelector(".nav-icon");
 
 btnNav.addEventListener("click", toggleMenu);
 
 function toggleMenu() {
-	body.classList.toggle("lock");
+	// body.classList.toggle("lock");
+	wrapper.classList.toggle("lock");
 	menu.classList.toggle("fixed");
 }
